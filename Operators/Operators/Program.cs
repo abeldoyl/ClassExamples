014fde7f14f4f9a77fd4dc44e129cc37dfdcf0f2
abeldoyl <abeldoyle71@gmail.com>
2026-01-20 08:26:18 -0700

    added equality operations for less than, greater than, and equal to

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -15,6 +15,13 @@
             Console.WriteLine(13.0 / 5); // floating point division
             Console.WriteLine(13 / 5.0); // floating point division
 
+            Console.WriteLine(5 > 2); // a is greater than b
+            Console.WriteLine(5 < 2); // a is less than b
+            Console.WriteLine(5 == 2); // a is equal to b
+            Console.WriteLine(5 != 2); // a is not equal to b
+            Console.WriteLine(5 >= 2); // a is greater than or equal to b
+            Console.WriteLine(5 <= 2); // a is less than or equal to b
+
             //pause
             Console.Read();
         }
