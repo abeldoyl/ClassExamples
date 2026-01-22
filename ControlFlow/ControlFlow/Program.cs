ad8455fa48a17939ea1f19a7380327db737a4585
abeldoyl <abeldoyle71@gmail.com>
2026-01-22 08:25:27 -0700

    add else statement

--- ControlFlow/ControlFlow/Program.cs
+++ ControlFlow/ControlFlow/Program.cs
@@ -7,10 +7,14 @@ namespace ControlFlow
     {
         static void Main(string[] args)
         {
-            int score = 60;
-            if (score >= 90)
+            int score = 59;
+            if (score >= 60)
             {
-                Console.WriteLine("Excellent! You got an A.");
+                Console.WriteLine("Pass. You did well enough.");
+            }
+            else
+            {
+                Console.WriteLine("Fail. Let's review and try again.");
             }
 
             //pause
