327ed5f01b72d432e7d2416ea115483d1c48d583
abeldoyl <abeldoyle71@gmail.com>
2026-01-22 08:21:49 -0700

    add simple if statement

---
+++ ControlFlow/ControlFlow/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+namespace ControlFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 60;
+            if (score >= 90)
+            {
+                Console.WriteLine("Excellent! You got an A.");
+            }
+
+            //pause
+            Console.Read();
+            
+            
+        }
+    }
+}
