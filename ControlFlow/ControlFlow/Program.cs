28bc5c00aa168c5d2f6c869f4f62de1968e9950e
abeldoyl <abeldoyle71@gmail.com>
2026-01-22 08:42:02 -0700

    switch funtion

--- ControlFlow/ControlFlow/Program.cs
+++ ControlFlow/ControlFlow/Program.cs
@@ -7,6 +7,7 @@ namespace ControlFlow
     {
         static void Main(string[] args)
         {
+            /*
             int score = 100;
             if (score >= 100)
             {
@@ -23,6 +24,24 @@ namespace ControlFlow
             else
             {
                 Console.WriteLine("Keep trying! Practice makes perfect.");
+            }
+            */
+
+            int day = 3;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("It's Monday. Time to start the week!");
+                    break;
+                case 2:
+                    Console.WriteLine("It's Tuesday. Keep going!");
+                    break;
+                case 3:
+                    Console.WriteLine("It's Wednesday. Halfway there!");
+                    break;
+                default:
+                    Console.WriteLine("It's another day. Make it count!");
+                    break;
             }
 
             //pause
