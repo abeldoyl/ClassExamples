7b7de7c70486084e2080230f123ed423d7fcd51f
abeldoyl <abeldoyle71@gmail.com>
2026-01-23 08:50:51 -0700

    add comments for while and do loops

--- LoopExample/LoopExample/Program.cs
+++ LoopExample/LoopExample/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            
+            /*
             for (int i = 0; i < 5; i++)
 
             {
@@ -15,22 +15,24 @@
             {
                 Console.WriteLine($"Count: {i}");
             }
-            
+            */
             
             int number = 1;
            
-            do
-            {
-                Console.WriteLine($"In Do Loop Number: {number}");
-                number++;
-            } while (number <= 3);
-            
+            //only runs if condition is true
             while (number <= 3)
 
             {
                 Console.WriteLine($"In While Loop Number: {number}");
                 number++;
             }
+
+            //always runs at least one time
+            do
+            {
+                Console.WriteLine($"In Do Loop Number: {number}");
+                number++;
+            } while (number <= 3);
 
             //pause
             Console.Read();
