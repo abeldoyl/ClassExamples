8025cf495232f295e405d8f7a557bb57f3ab1f2e
abeldoyl <abeldoyle71@gmail.com>
2026-01-23 08:48:56 -0700

    add for loops, while loops, and do loops

---
+++ LoopExample/LoopExample/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LoopExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i < 5; i++)
+
+            {
+                Console.WriteLine($"Count: {i}");
+            }
+
+            for (int i = 5; i >= -5; i--)
+            {
+                Console.WriteLine($"Count: {i}");
+            }
+            
+            
+            int number = 1;
+           
+            do
+            {
+                Console.WriteLine($"In Do Loop Number: {number}");
+                number++;
+            } while (number <= 3);
+            
+            while (number <= 3)
+
+            {
+                Console.WriteLine($"In While Loop Number: {number}");
+                number++;
+            }
+
+            //pause
+            Console.Read();
+        }
+    }
+}
