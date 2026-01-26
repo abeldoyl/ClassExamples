0bc828af156e66dc0d494ca980607468574f54c2
abeldoyl <abeldoyle71@gmail.com>
2026-01-26 08:53:56 -0700

    loop intil value or quit

--- Exceptions/Exceptions/Program.cs
+++ Exceptions/Exceptions/Program.cs
@@ -4,24 +4,52 @@
     {
         static void Main(string[] args)
         {
-            string userInput = "";
-            int result = 0;
-            Console.WriteLine("Please enter a number");
-            userInput = Console.ReadLine();
-            Console.WriteLine($"You entered {userInput}");
-            try
             {
-                // risky code
-            result = int.Parse(userInput);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Something went wrong: " + ex.StackTrace);
-            }
+                string userInput = "";
+                int result = 0;
+                bool isValid = false;
+                string message = "";
+
+                do
+                {
+                    Console.WriteLine("Please enter a number");
+                    Console.WriteLine("Enter \"Q\" to quit");
+                    userInput = Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine($"You entered {userInput}");
+
+                    try
+                    {
+                        // risky code
+                        result = int.Parse(userInput);
+                        isValid = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        if (userInput == "Q" || userInput == "q"
+                            )
+                        {
+                            message = $"Have a nice day!";
+                            isValid = true;
+                        }
+                        else
+                        {
+                            message = $"{userInput} is not a number";
+                        }
+                    }
+
+                }
+                while (!isValid);
+                message = $"{result} is a number";
 
 
-            //pause
-            Console.ReadLine();
+
+                //pause
+                Console.ReadLine();
+
+            }
+
         }
+
     }
 }
