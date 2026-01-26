28e52e0ee25ea8349f40994b4592c9560f57f401
abeldoyl <abeldoyle71@gmail.com>
2026-01-26 08:29:51 -0700

    simple exception example

--- Exceptions/Exceptions/Program.cs
+++ Exceptions/Exceptions/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput = "";
+            int result = 0;
+            Console.WriteLine("Please enter a number");
+            userInput = Console.ReadLine();
+            Console.WriteLine($"You entered {userInput}");
+            try
+            {
+                // risky code
+            result = int.Parse(userInput);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong: " + ex.StackTrace);
+            }
+
 
             //pause
             Console.ReadLine();
