8c135a2830cf55fa8a611b9fb48a90779d1ef2cc
abeldoyl <abeldoyle71@gmail.com>
2026-01-27 08:47:12 -0700

    random number generator

--- RandomNumbers/RandomNumbers/Program.cs
+++ RandomNumbers/RandomNumbers/Program.cs
@@ -4,9 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-            int n = rand.Next(1, 7); // Random number between 1 and 6
-            Console.WriteLine(n);
+            //test randomness and range with a for loop
+            for (int i = 0; i < 1000; i++)
+            {
+                //rand.Next(min,Max) max is not incuded
+                Random rand = new Random();
+                int n = rand.Next(0, 2); // Random number between 1 and 6
+                
+                Console.Write(n);
+            }
 
             //pause
             Console.Read();
