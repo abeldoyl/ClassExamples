762eda91334b81d7ed7e4431806db5a170220fae
abeldoyl <abeldoyle71@gmail.com>
2026-01-28 08:51:57 -0700

    add grid values

--- ArrayExamples/ArrayExamples/Program.cs
+++ ArrayExamples/ArrayExamples/Program.cs
@@ -5,9 +5,11 @@
         static void Main(string[] args)
         {
             int thing = 0;
-            
             string[] fruits = new string[7];
-            
+            string[] daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            int[,] grid = new int[5, 5];
+
+            /*
             fruits[0] = "apple";
             fruits[1] = "banana";
             fruits[2] = "pear";
@@ -29,6 +31,18 @@
             }
 
             Console.WriteLine($"the length of the fruit is: {fruits.Length}");
+            */
+
+            for (int colum = 0; colum < 5; colum++)
+            {
+                for (int row = 0; row < 5; row++)
+                {
+                    grid[colum, row] = row * colum;
+                }
+            }
+
+                    Console.Write(grid[3,3]);
+
 
             //pause
             Console.Read();
