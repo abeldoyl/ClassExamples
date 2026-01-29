8ac1835a83d65f68368b0006e3b68d88fbfb815f
abeldoyl <abeldoyle71@gmail.com>
2026-01-29 08:40:28 -0700

    add method

--- MethodExamples/MethodExamples/Program.cs
+++ MethodExamples/MethodExamples/Program.cs
@@ -4,9 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int count = 5;
+            
+            AddNumbers();
+            SayHello();
+            ShowCount();
+            Console.WriteLine(count);
             //pause
             Console.Read();
+        }
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void AddNumbers()
+        {
+            Console.WriteLine(1 + 1);
+        }
+
+        static void ShowCount()
+        {
+            int count = 7;
+            Console.WriteLine(count);
         }
     }
 }
