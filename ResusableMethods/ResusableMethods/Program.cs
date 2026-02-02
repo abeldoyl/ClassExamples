668306f7903ea3ef3d6c265a5b0d6a1b7f635893
abeldoyl <abeldoyle71@gmail.com>
2026-02-02 08:55:42 -0700

    add random numbers into a nice array for max to min values

--- ResusableMethods/ResusableMethods/Program.cs
+++ ResusableMethods/ResusableMethods/Program.cs
@@ -4,8 +4,41 @@
     {
         static void Main(string[] args)
         {
+            TestRandomNumber();
             //pause
             Console.Read();
+        }
+
+        static void TestRandomNumber()
+        {
+            int[] counts = new int[12];
+            int max = 10, min = 5, currentNumber = 0, padding = 8;
+            
+            for (int i = 0; i < 10000; i++)
+            {
+                //Console.Write(RandomNumberBetween(10, 5).ToString().PadLeft(2));
+                currentNumber = RandomNumberBetween(max,min);
+                counts[currentNumber] += 1;
+            }
+            for (int i = 0; i < counts.Length; i++)
+            {
+                Console.Write(i.ToString().PadLeft(padding) + "|");
+            } 
+            Console.WriteLine();
+            //Makes into colums
+            foreach (int count in counts)
+            { 
+                Console.Write(count.ToString().PadLeft(padding) + "|");
+            }
+        }
+        
+        static int RandomNumberBetween(int max, int min)
+        {
+            int range = max - min + 1;
+            Random rand = new Random();
+            int randomNumber = rand.Next(range);
+            randomNumber += min; // shift value back up to the range between max and min
+            return randomNumber;
         }
     }
 }
