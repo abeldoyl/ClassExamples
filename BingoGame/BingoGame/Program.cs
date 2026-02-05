6d6b5354c5ef32168ead3eee20d4698b92cea103
abeldoyl <abeldoyle71@gmail.com>
2026-02-05 08:43:06 -0700

    todo list for bingo game

---
+++ BingoGame/BingoGame/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * TODO:
+ * [ ] Track bingo balls drawn in a two dimentional array
+ * [ ] Display status of all balls on the console
+ * [ ] Clear all drawn balls to start a new game
+ * [ ] Let the user quit
+ * [ ] Draw a random ball
+ * [ ] Get a random number to determine ball letter
+ * [ ] Get a random number to determine ball number
+ * [ ] Check if ball has already been drawn. 
+ *     No: mark as drawn, Yes: draw another
+*/
+namespace BingoGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] drawnBalls = new bool[5, 15];
+
+            drawnBalls[1,0] = true;
+            //pause
+            Console.Read();
+        }
+    }
+}
