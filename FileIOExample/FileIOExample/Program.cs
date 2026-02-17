1105c9f8678168d173f710ed4f4721907857b1e6
abeldoyl <abeldoyle71@gmail.com>
2026-02-17 08:52:22 -0700

    add write, append, and read file functions

--- FileIOExample/FileIOExample/Program.cs
+++ FileIOExample/FileIOExample/Program.cs
@@ -6,8 +6,40 @@
         {
             Console.WriteLine("Hello, World!");
 
+            //WriteToFile();
+            //AppendToFile();
+            ReadFile();
             //pause
             Console.Read();
+        }
+
+        static void WriteToFile()
+        {
+            using (StreamWriter testFile = File.CreateText("..\\..\\..\\TestFile.txt"))
+            //using ..\ moves the file up a level in file manager the second \ is to
+            //make the code read the \ as a comand not as a comment
+            
+            {
+                testFile.WriteLine("Wake up Neo...");
+            }
+        }
+
+        static void AppendToFile()
+        {
+            using (StreamWriter testFile = File.AppendText("..\\..\\..\\TestFile.txt"))
+            {
+                testFile.WriteLine("Follow the white rabbit...");
+            }
+        }
+
+        static void ReadFile()
+        {
+            using (StreamReader testFile = new StreamReader("..\\..\\..\\TestFile.txt"))
+            {
+                
+                Console.WriteLine(testFile.ReadLine());
+                Console.WriteLine(testFile.ReadLine());
+            }
         }
     }
 }
