9f78a2eae3ee6facaa681ed5c67d3664f7e7e5ba
abeldoyl <abeldoyle71@gmail.com>
2026-04-02 08:26:26 -0600

    add picture box, anchored to all sides for auto scaling

--- GraphicExample/GraphicExample/Graphics.Designer.cs
+++ GraphicExample/GraphicExample/Graphics.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.BackColor = Color.WhiteSmoke;
+            pictureBox1.Location = new Point(12, 39);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1458, 808);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Graphics
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1482, 953);
+            Controls.Add(pictureBox1);
+            MinimumSize = new Size(500, 500);
+            Name = "Graphics";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
     }
 }
