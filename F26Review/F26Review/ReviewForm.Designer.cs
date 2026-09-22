934e3646d429ee8823cb74741a2db30d58cb0a9e
abeldoyl <abeldoyle71@gmail.com>
2026-09-22 09:56:50 -0600

    added exit button with functionality

--- F26Review/F26Review/ReviewForm.Designer.cs
+++ F26Review/F26Review/ReviewForm.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            ExitButton = new Button();
+            SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            ExitButton.Location = new Point(691, 365);
+            ExitButton.Name = "ExitButton";
+            ExitButton.Size = new Size(97, 73);
+            ExitButton.TabIndex = 0;
+            ExitButton.Text = "E&xit";
+            ExitButton.UseVisualStyleBackColor = true;
+            ExitButton.Click += this.ExitButton_Click;
+            // 
+            // ReviewForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(ExitButton);
+            Name = "ReviewForm";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button ExitButton;
     }
 }
